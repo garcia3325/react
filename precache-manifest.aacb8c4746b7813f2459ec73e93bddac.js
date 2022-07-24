72ef6c4137a4caa200bd13de4213153fa8cd499a
Rene Garcia Jr <renegarciajr.@Renes-Air.attlocal.net>
2022-07-24 13:33:00 -0400

    Updates

---
+++ precache-manifest.aacb8c4746b7813f2459ec73e93bddac.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbbc843f15e268ec6700",
+    "url": "/react/static/css/main.3d5afb6a.chunk.css"
+  },
+  {
+    "revision": "dbbc843f15e268ec6700",
+    "url": "/react/static/js/main.dbbc843f.chunk.js"
+  },
+  {
+    "revision": "3509f893052e151b68de",
+    "url": "/react/static/css/1.10d3c0ea.chunk.css"
+  },
+  {
+    "revision": "3509f893052e151b68de",
+    "url": "/react/static/js/1.3509f893.chunk.js"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "5614807d54295c07855b49aa204912d5",
+    "url": "/react/static/media/img1.5614807d.png"
+  },
+  {
+    "revision": "fc0271044adb075245f4c5e3d56ed1e9",
+    "url": "/react/static/media/img0.fc027104.png"
+  },
+  {
+    "revision": "a80ee3c32080cf96f705d2735829e050",
+    "url": "/react/static/media/img4.a80ee3c3.png"
+  },
+  {
+    "revision": "d79f37218e660db2b4e81b83e33f93eb",
+    "url": "/react/static/media/Img6.d79f3721.png"
+  },
+  {
+    "revision": "f7db251be6b0dcd984a704e80f4270ae",
+    "url": "/react/static/media/img3.f7db251b.png"
+  },
+  {
+    "revision": "ecf19f0c68455767d1f5922eff92282d",
+    "url": "/react/static/media/img5.ecf19f0c.png"
+  },
+  {
+    "revision": "8de55fe29edc69962862e56eec0f37e2",
+    "url": "/react/index.html"
+  }
+];
